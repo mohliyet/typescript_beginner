0786f53c8294cbcb6b395fba36513a0b0d2421ab
M. A. <mohaliyeu@gmail.com>
2024-05-07 15:55:25 +0300

    typescript programming challenge started

---
+++ src/tutorial.ts
@@ -0,0 +1,13 @@
+console.log('Hello TypeScript!');
+
+interface someValue {
+    name: string;
+    id: number;
+}
+
+let someObj: someValue = {
+    name: 'Mohammed Aliy',
+    id: 123
+};
+
+console.log(someObj);
