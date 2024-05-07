55377e04773a1e240ed21ccf638fb37dad5572be
M. A. <mohaliyeu@gmail.com>
2024-05-07 17:46:14 +0300

    some simple challenges about ts were conducted.

--- src/objects_and_functions.ts
+++ src/objects_and_functions.ts
@@ -10,5 +10,18 @@ let notebook = {title: 'spiral'};
 
 let items: {readonly title: string; cost?: number}[] = [book, pen, notebook]; // cost is optional and title is readonly
 
-// items[0].title = 'JavaScript';
+// items[0].title = 'JavaScript'; // Error: Cannot assign to 'title' because it is a read-only property
+
+
+// Challnge #04
+
+let bike: {brand: string; year: number} = {brand: 'Honda', year: 2020};
+let laptop: {brand: string; year: number} = {brand: 'Apple', year: 2021};
+
+let product1 = {title: 'shirt', cost: 20};
+let product2 = {title: 'pants'}
+
+let products: {title: string; cost?: number}[] = [product1, product2];
+
+// products.push({title: 'shoes', cost: '30'}); // Error: Argument of type '{title: string; cost: string;}' is not assignable to parameter of type '{title: string; cost?: number;}'
 
