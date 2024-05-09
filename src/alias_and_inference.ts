bdd8aac20aaba7f6e1caedfe97552e8ad8005086
M. A. <mohaliyeu@gmail.com>
2024-05-09 16:39:25 +0300

    challnge about type alias

--- src/alias_and_inference.ts
+++ src/alias_and_inference.ts
@@ -67,3 +67,32 @@ function setTheme(theme: Theme) {
 
 // Call the 'setTheme' function with 'light' as the argument.
 setTheme('light');
+
+// challenge #07
+
+/*
+- define the Employee type: Create a type employee with properties id (number), name (string), and department (string).
+    type Employee = {id: number, name: string, department: string};
+- define the Manager type: Create a type Manager with properties id (number), name (string), and employees (an array of Employee).
+    type Manager = {id: number, name: string, employees: Employee[]};
+- Create a Union Type: Define a type Staff that is a union of Employee and Manager.
+    type Union = Employee | Manager;
+- Create the printStaffDetails function: This function should accept a parameter of type Staff. Inside the function, use a 
+type guard to check if the 'employees' property exists in the passed object. If it does, print 
+a message indicating that the person is a manager and the number of employees they manage. If it doesn't, print a message indicating that
+the person is a manager and the number of employees they manage. If it doesn't, print a message indicating that the person is an employee and 
+the department they belong to.
+
+*/
+
+type Employee = {id: number, name: string, department: string};
+type Manager = {id: number, name: string, employees: Employee[]};
+type Union = Employee | Manager;
+
+function printStaffDetails(staff: Union) {
+    if ('employees' in staff) {
+        console.log(`${staff.name} is a manager and manages ${staff.employees.length} employees.`);
+    } else {
+        console.log(`${staff.name} is an employee in the ${staff.department} department.`);
+    }
+}
